3d8ca18607533a635eb397193295d79e2a3763c2
Todd Beets <todd@beetsme.net>
2023-08-14 16:40:05 -0700

    Add a version of ClientConnect that takes a url string instead of an active server reference.

--- client.go
+++ client.go
@@ -19,6 +19,19 @@ func ClientConnect(t testing.TB, s *server.Server, opts ...nats.Option) *nats.Co
 	return nc
 }
 
+// ClientConnectURL creates a client connection from passed NATS URL taking optional options
+// e.g. user credentials nats.UserCredentials("path/to/creds") or nats.UserInfo("user", "password")
+func ClientConnectURL(t testing.TB, url string, opts ...nats.Option) *nats.Conn {
+	t.Helper()
+	nc, err := nats.Connect(url, opts...)
+	if err != nil {
+		t.Fatalf("Failed to create client: %v", err)
+	}
+
+	return nc
+}
+
 var (
 	_ = ClientConnect
+	_ = ClientConnectURL
 )
